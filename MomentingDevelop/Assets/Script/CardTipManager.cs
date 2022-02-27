07a10fde4abd8cd703bcb5e2d54478dbd281858e
youngKairos <mjdcd1899@gmail.com>
2022-02-27 15:12:46 +0900

    카드팁매니저 스크립트 내용 일부 수정

--- MomentingDevelop/Assets/Script/CardTipManager.cs
+++ MomentingDevelop/Assets/Script/CardTipManager.cs
@@ -2,7 +2,6 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-using System;
 
 public class CardTipManager : MonoBehaviour
 {
@@ -25,10 +24,10 @@ public class CardTipManager : MonoBehaviour
     {
         //서비스팁 텍스트가 담긴 배열 생성
         CardTipArr = new string[TextMaxCount];
-        CardTipArr[0] = "#0# 단체 채팅 시간이 다 끝나면" + "\n" + "1:1 대화 상대를 선택할 수 있어요!" + "\n" + "종료된 이후 단체방은 폭파되니" + "\n" + "연락하기 위해서는 서로 선택해야 해요.";
-        CardTipArr[1] = "#1# 1:1 대화 신청을 수락하면 상대의" + "\n" + "다른 프로필 사진들을 확인할 수 있어요.";
-        CardTipArr[2] = "#2# 1:1 대화 24시간 경과 시" + "\n" + "매너 평가를 할 수 있어요." + "\n" + "남겨주신 평가는 상대의" + "\n" + "매너 등급에 반영됩니다!";
-        CardTipArr[3] = "#3# 1:1 대화방을 삭제하면 대화가 종료되고" + "\n" + "7일 후에 대화방이 자동 삭제돼요.";
+        CardTipArr[0] = "단체 채팅 시간이 다 끝나면" + "\n" + "1:1 대화 상대를 선택할 수 있어요!" + "\n" + "종료된 이후 단체방은 폭파되니" + "\n" + "연락하기 위해서는 서로 선택해야 해요.";
+        CardTipArr[1] = "1:1 대화 신청을 수락하면 상대의" + "\n" + "다른 프로필 사진들을 확인할 수 있어요.";
+        CardTipArr[2] = "1:1 대화 24시간 경과 시" + "\n" + "매너 평가를 할 수 있어요." + "\n" + "남겨주신 평가는 상대의" + "\n" + "매너 등급에 반영됩니다!";
+        CardTipArr[3] = "1:1 대화방을 삭제하면 대화가 종료되고" + "\n" + "7일 후에 대화방이 자동 삭제돼요.";
 
         //Tip 게임오브젝트 찾고 연결하기
         GameObject Tip = GameObject.Find("Tip");
