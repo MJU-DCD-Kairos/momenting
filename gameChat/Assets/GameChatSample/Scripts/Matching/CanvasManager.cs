625e7b05052cba6f85d55185d3c4e6e277d102e5
youngKairos <mjdcd1899@gmail.com>
2022-03-04 22:20:25 +0900

    채팅방리스트 더보기, 편집 화면 작업
    캔버스 전환, 편집 화면 토글 버튼 배치

--- gameChat/Assets/GameChatSample/Scripts/Matching/CanvasManager.cs
+++ gameChat/Assets/GameChatSample/Scripts/Matching/CanvasManager.cs
@@ -4,31 +4,16 @@ using UnityEngine;
 
 public class CanvasManager : MonoBehaviour
 {
-    public GameObject Canvas_back;
-    public GameObject Canvas_next;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public GameObject Canvas_inactive;
+    public GameObject Canvas_active;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 
-    public void Back()
+    public void Swtich()
     {
-        Canvas_back.SetActive(true);
-        Canvas_next.SetActive(false);
+        Canvas_inactive.SetActive(false);
+        Canvas_active.SetActive(true);
 
     }
 
-    public void Next()
-    {
-        Canvas_back.SetActive(false);
-        Canvas_next.SetActive(true);
-
-    }
+    
 }
