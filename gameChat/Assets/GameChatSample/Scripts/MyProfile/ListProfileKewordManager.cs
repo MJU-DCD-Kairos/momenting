023fdc72d29c1e793c91c0456ebb81217ed725f5
HyerinYang <ulysia0402@gmail.com>
2022-03-11 16:56:20 +0900

    키워드 리스트 매니저 스크립트 수정, 키워드 프리팹 수정

---
+++ gameChat/Assets/GameChatSample/Scripts/MyProfile/ListProfileKewordManager.cs
@@ -0,0 +1,70 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+[System.Serializable]
+
+public class List
+{
+    public List(string _KeywordName, string _Description)
+    { KeywordName = _KeywordName; Description = _Description; }
+
+    public string KeywordName, Description;
+    public bool Badge_SameKeword;
+}
+
+public class ListProfileKewordManager : MonoBehaviour
+{
+    //txt파일을 인스펙터에서 참조하도록 생성
+    public TextAsset KeywordData;
+
+    //리스트 배열 생성
+    public List<List> KeywordList;
+
+    //리스트를 생성할 부모 오브젝트를 참조
+    public GameObject Content;
+
+
+    void Start()
+    {
+        LoadList();
+
+    }
+
+    public void LoadList()
+    {
+        //리스트 불러오기
+        string[] line = KeywordData.text.Substring(0, KeywordData.text.Length - 1).Split('\n');
+        for (int i = 0; i < line.Length; i++)
+        {
+            string[] row = line[i].Split('\t');
+
+            KeywordList.Add(new List(row[0], row[1]));
+
+            //리스트 프리팹을 Content 부모 밑에 자식으로 생성
+            GameObject ListContent = Instantiate(Resources.Load("Prefabs/Keyword")) as GameObject;
+            ListContent.transform.SetParent(Content.transform, false);
+            GameObject[] KeywordObj = GameObject.FindGameObjectsWithTag("Keyword");
+            KeywordObj[i].GetComponent<Text>().text = row[0];
+
+            GameObject[] DetailObj = GameObject.FindGameObjectsWithTag("Detail");
+            DetailObj[i].GetComponent<Text>().text = row[1];
+
+            Debug.Log(row[0]);
+            Debug.Log(row[1]);
+        }
+    }
+
+    public void ToggleIsOn(bool isOn)
+    {
+        if (isOn)
+        {
+
+        }
+
+    }
+
+
+}
