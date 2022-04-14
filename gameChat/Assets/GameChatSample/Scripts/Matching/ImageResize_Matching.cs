b090209db0f9a4c5c030be2fd53be72e43665e71
youngKairos <mjdcd1899@gmail.com>
2022-04-14 15:25:30 +0900

    매칭대기 화면 이미지 리사이즈 스크립트 수정

--- gameChat/Assets/GameChatSample/Scripts/Matching/ImageResize_Matching.cs
+++ gameChat/Assets/GameChatSample/Scripts/Matching/ImageResize_Matching.cs
@@ -5,7 +5,7 @@ using UnityEngine.UI;
 
 public class ImageResize_Matching : MonoBehaviour
 {
-    //������Ʈ�� ���� �ν����ͻ󿡼� ������ ���� �����ϵ��� ����
+    //컴포넌트에 따라 인스펙터상에서 사이즈 조절 가능하도록 선언
     public float size = 384;
     public RectTransform rt;
 
@@ -20,20 +20,19 @@ public class ImageResize_Matching : MonoBehaviour
     // Update is called once per frame
     public void Update()
     {
-        //���� ���ϱ� ����
-        //�ҽ��̹��� ���� ������� ����
+        //소스이미지 원래 사이즈로 세팅
         GetComponent<Image>().SetNativeSize();
 
         var RectTransform = transform as RectTransform;
 
-        //���̰� �� ª���� ���̸� �ν����ͻ󿡼� �Է��� size�� �ٲ�
+        //넓이가 더 짧으면 넓이를 인스펙터상에서 입력한 size로 바꿈
         if (RectTransform.sizeDelta.x < RectTransform.sizeDelta.y)
         {
             float aspect = RectTransform.sizeDelta.y / RectTransform.sizeDelta.x; //�̹��� ���� ���ϱ�
-            
+            RectTransform.sizeDelta = new Vector2(size, size * aspect);
         }
 
-        //���̰� �� ª���� ���̸� �ν����ͻ󿡼� �Է��� size�� �ٲ�
+        //높이가 더 짧으면 높이를 인스펙터상에서 입력한 size로 바꿈
         else
         {
             float aspect = RectTransform.sizeDelta.x / RectTransform.sizeDelta.y; //�̹��� ���� ���ϱ�
