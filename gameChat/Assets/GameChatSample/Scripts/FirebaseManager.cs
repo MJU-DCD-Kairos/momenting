2408588a47c619c53fbfcdb0e2d464b5e22c5399
youngKairos <mjdcd1899@gmail.com>
2022-05-19 17:22:42 +0900

    파이어베이스 스크립트 업로드

--- gameChat/Assets/GameChatSample/Scripts/FirebaseManager.cs
+++ gameChat/Assets/GameChatSample/Scripts/FirebaseManager.cs
@@ -1,4 +1,4 @@
-using System.Collections;
+
 using System.Collections.Generic;
 using UnityEngine;
 using Firebase;
@@ -8,11 +8,10 @@ using UnityEngine.UI;
 using System.Threading;
 using System.Threading.Tasks;
 
-namespace FireStoreScript
-{
+namespace FirestoreScript {
     public class FirebaseManager : MonoBehaviour
     {
-        public FirebaseFirestore db;
+        public static FirebaseFirestore db;
 
         public InputField Name;
         public InputField Age;//db 생성 테스트를 위한 아이디 인풋필드
@@ -29,6 +28,7 @@ namespace FireStoreScript
         public bool isActive;
         public bool isInMatchingDB;
 
+        public enum fbRef { userInfo, matchingRoom, report, userToken, keywords, chatRoom, images, mannerRate}
         public void Start()
         {
             //씬매니저 파괴 방지를 위한 코드
@@ -36,13 +36,13 @@ namespace FireStoreScript
 
             Firebase.FirebaseApp.CheckAndFixDependenciesAsync().ContinueWith(task =>
             {
-                //var dependencyStatus = task.Result;
-                if (task.Result == DependencyStatus.Available)
+            //var dependencyStatus = task.Result;
+            if (task.Result == DependencyStatus.Available)
                 {
                     Debug.Log("파이어스토어 DB 연결 성공");
                     db = FirebaseFirestore.DefaultInstance; //Cloud Firestore 인스턴스 초기화
 
-                }
+            }
                 else
                 {
                     Debug.LogError("Could not resolve all Firebase dependencies: " + task.Result);
@@ -50,14 +50,13 @@ namespace FireStoreScript
             });
 
             //isMatchToken(); //토큰 정보 있는지 확인
-            Debug.Log(PlayerPrefs.GetString("name"));
+            //Debug.Log("유저닉네임 : " + PlayerPrefs.GetString("name"));
+            //Debug.Log("유저성별 : " + PlayerPrefs.GetString("sex"));
 
         }
 
         public void myName() { inputName(Name.text); }//inputID 함수 호출
-        public void myage() { inputAge(Age.text); }
         public void mySex() { inputSex(Sex.text); }
-        public void myToken() { inputToken(Token.text); }
         public void inputName(string text) //아이디 인풋 데이터 받아오기
         {
             if (text.Trim() == "")
@@ -68,19 +67,8 @@ namespace FireStoreScript
             else
             {
                 Debug.Log("유저아이디: " + Name.text);
-            }
 
-        }
-        public void inputAge(string text) //아이디 인풋 데이터 받아오기
-        {
-            if (text.Trim() == "")
-            {
-                Debug.Log("나이를 입력하지 않았습니다.");
-                return;
-            }
-            else
-            {
-                Debug.Log("유저나이: " + Age.text);
+                myname = Name.text;
             }
 
         }
@@ -94,34 +82,20 @@ namespace FireStoreScript
             else
             {
                 Debug.Log("유저성별: " + Sex.text);
-            }
-
-        }
-        public void inputToken(string text) //아이디 인풋 데이터 받아오기
-        {
-            if (text.Trim() == "")
-            {
-                Debug.Log(" 토큰 ");
-                return;
-            }
-            else
-            {
-                Debug.Log("유저토큰: " + Token.text);
+                sex = Sex.text;
             }
 
         }
         public async void isMatch_LoadData()
-        {//매칭버튼 누르기 전에 인풋필드에 닉네임 입력 먼저 하기 (닉네임으로 조회)
-
+        {
+            myname = Name.text;
             await LoadData(); //유저 정보 불러오기
             LocalData(); //유저 정보 로컬 저장
-            isMatchToken();
+            //await isMatchToken();
         }
 
         private async Task LoadData() //파이어스토어DB에서 유저정보 불러오는 함수
         {
-            myname = Name.text;
-
             Query userRef = db.Collection("userInfo").WhereEqualTo("name", myname); //입력한 닉네임과 일치하는 쿼리 찾아서 참조
 
             if (userRef != null) //유저 정보가 있으면
@@ -132,12 +106,17 @@ namespace FireStoreScript
                     foreach (DocumentSnapshot doc in snapshot.Documents)
                     {
                         Dictionary<string, object> docDictionary = doc.ToDictionary();
-                        //myname = docDictionary["name"] as string;
+                        myname = docDictionary["name"] as string;
                         sex = docDictionary["sex"] as string;
-                        Debug.Log("성별 제대로 불러와지는지: " + docDictionary["sex"] as string);
-                    }
-                    //Debug.Log("닉네임 : " + myname);
-                    //Debug.Log("성별 : " + sex);
+                    //age = int.Parse(docDictionary["age"] as string);
+                    //mbti = docDictionary["mbti"] as string;
+                    //mannerLevel = int.Parse(docDictionary["mannerLevel"] as string);
+                    Debug.Log(myname + "의 성별 불러오기 성공 -> " + docDictionary["sex"] as string);
+                    //token = docDictionary["token"] as string;
+                    //return;
+                }
+                    Debug.Log("닉네임 : " + myname);
+                    Debug.Log("성별 : " + sex);
                 });
 
             }
@@ -149,68 +128,57 @@ namespace FireStoreScript
 
         void LocalData() //유저데이터 로컬 저장 (가입단에 들어감)
         {
+            Debug.Log("LocalData 함수 실행됨");
+
             PlayerPrefs.SetString("name", myname); //이름
             PlayerPrefs.SetString("sex", sex); //성별
             PlayerPrefs.SetInt("age", age); //나이
             PlayerPrefs.SetString("mbti", mbti); //모래알유형
             PlayerPrefs.SetInt("mannerLevel", mannerLevel); //매너등급
-            isInMatchingDB = false;
-            PlayerPrefs.SetString("isInMatchingDB", isInMatchingDB.ToString());
 
             PlayerPrefs.Save();
+
         }
 
-        async void isMatchToken() //토큰 DB에서 정보 확인 (가입단에 들어감) 
+        private async Task isMatchToken() //토큰 DB에서 정보 확인 (가입단에 들어감) 
         {
-            //Query allTokenQuery = null;
+            CollectionReference userTokenColl = db.Collection("userToken");
+            Query allTokenQuery = userTokenColl.WhereEqualTo("name", PlayerPrefs.GetString("name")); //닉네임으로 일치하는 쿼리 찾아서 참조
 
-            Query allTokenQuery = db.Collection("userToken").WhereEqualTo("name", PlayerPrefs.GetString("name")); //닉네임으로 일치하는 쿼리 찾아서 참조
-            await allTokenQuery.GetSnapshotAsync().ContinueWithOnMainThread(task =>
+            if (allTokenQuery != null)
             {
-                QuerySnapshot allToken = task.Result;
-                foreach (DocumentSnapshot token in allToken.Documents)
+                await allTokenQuery.GetSnapshotAsync().ContinueWithOnMainThread(task =>
                 {
-                    Dictionary<string, object> tokenDictionary = token.ToDictionary();
-
-                    if (tokenDictionary["token"] as string != null) //토큰 정보가 있으면
-                    {
-                        Debug.Log(string.Format(tokenDictionary["name"] as string));
-                        Debug.Log(string.Format(tokenDictionary["token"] as string));
-                    }
-
-                    else //토큰 정보 없으면 토큰DB에 새로 추가
-                    {
-                        CollectionReference tokenColl = db.Collection("userToken"); //토큰 콜렉션 참조
-                        Dictionary<string, object> update = new Dictionary<string, object> //새로운 딕셔너리 생성
-                    {
-                        {"name", PlayerPrefs.GetString("name") },
-                        {"token" , PlayerPrefs.GetString("token") }
-                    };
-                        tokenColl.AddAsync(update);
-                    }
-
-
-                    /*
-                    if (allToken != null) //토큰 정보가 있으면
+                    QuerySnapshot allToken = task.Result;
+                    foreach (DocumentSnapshot token in allToken.Documents)
                     {
                         Dictionary<string, object> tokenDictionary = token.ToDictionary();
-                        Debug.Log(string.Format(tokenDictionary["name"] as string));
-                        Debug.Log(string.Format(tokenDictionary["token"] as string));
-                    }
-                    else //토큰 정보 없으면 토큰DB에 새로 추가
-                    {
-                        CollectionReference tokenColl = db.Collection("userToken"); //토큰 콜렉션 참조
-                        Dictionary<string, object> update = new Dictionary<string, object> //새로운 딕셔너리 생성
-                        {
-                            {"name", PlayerPrefs.GetString("name") },
-                            {"token" , PlayerPrefs.GetString("token") }
-                        };
-                        tokenColl.AddAsync(update);
-                    }*/
-                }
+                        Debug.Log(string.Format("유저" + tokenDictionary["name"] as string + "의 토큰: " + tokenDictionary["token"] as string));
 
-            });
+                        Debug.Log("사용자" + myname + "의 토큰 일치 확인됨");
+                    }
+
+                });
+            }
+            else //토큰 정보 없으면 토큰DB에 새로 추가
+            {
+                Dictionary<string, object> newTokenDic = new Dictionary<string, object>
+            {
+                {"name" , myname },
+                {"token" , PlayerPrefs.GetString("token") }
+            };
+
+                DocumentReference newTokenDoc = userTokenColl.Document();
+                await newTokenDoc.SetAsync(newTokenDic);
+
+                Debug.Log("토큰 DB 문서 생성됨");
+            };
+
+            Debug.Log("isMatchToken 함수 실행됨");
         }
+
+
+
 
 
         public void makeUserData() //새로운유저 DB 생성
@@ -254,8 +222,8 @@ namespace FireStoreScript
 
             db.Collection("userInfo").Document(myname).SetAsync(user).ContinueWithOnMainThread(task =>
             {
-                //Debug.Log(string.Format("추가된 문서 ID: {0}.", addUserInfo.Id));
-            });
+            //Debug.Log(string.Format("추가된 문서 ID: {0}.", addUserInfo.Id));
+        });
 
         }
 
@@ -270,13 +238,17 @@ namespace FireStoreScript
                 {"isSend" , null  },
                 {"matchingState", null },
                 {"matchTime", null }
+
             });
         }
         public void chatDB() //채팅 DB 
         {
+
         }
+
         public void ImgDB() //사진 DB
         {
+
         }
         public void profileDB() //기본프로필 DB 생성 (닉네임,나이,성별,한줄소개,매너등급)
         {
@@ -312,7 +284,9 @@ namespace FireStoreScript
                 {"kw5_name", null },
                 {"kw5_description", null },
                 {"kw5_ratio", null }
+
             });
+
         }
         public void reportDB() //신고 DB
         {
@@ -322,11 +296,14 @@ namespace FireStoreScript
             {
                 {"uid", null },
                 {"reason" , null }
+
             });
         }
         public void mbtiType() //모래알유형 DB
         {
+
         }
+
         public void mannerDB()
         {
             string docname = ID.text + "mannerRate"; //문서 id 지정
@@ -335,6 +312,7 @@ namespace FireStoreScript
             {
                 {"uid", null },
                 {"score" , null }
+
             });
         }
         */
@@ -371,5 +349,9 @@ namespace FireStoreScript
             //해당하는 도큐먼트에서 channel ID를 반환하는 함수를 작성해야함
             //doc.TryGetValue<>(, )
         }*/
+
     }
 }
+
+
+
