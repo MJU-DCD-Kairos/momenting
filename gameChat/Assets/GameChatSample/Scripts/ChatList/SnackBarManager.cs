9c15feda5fd39253934c118b8b2f5b0068ff32de
youngKairos <mjdcd1899@gmail.com>
2022-05-25 04:53:03 +0900

    신고 기능, UI 작업

--- gameChat/Assets/GameChatSample/Scripts/ChatList/SnackBarManager.cs
+++ gameChat/Assets/GameChatSample/Scripts/ChatList/SnackBarManager.cs
@@ -5,18 +5,29 @@ using UnityEngine.UI;
 
 public class SnackBarManager : MonoBehaviour
 {
-    public float DestroyTime = 2.5f;
-    public GameObject Canvas_PersonalSeeMore;
+    public float DestroyTime = 4.3f;
+    public GameObject Canvas;
 
-    private void Start()
-    {
-        
-    }
     public void Sb_DeleteList()
     {
         GameObject snackbar = Instantiate(Resources.Load("Prefabs/SnackBar")) as GameObject;
-        snackbar.transform.SetParent(Canvas_PersonalSeeMore.transform, false);
+        snackbar.transform.SetParent(Canvas.transform, false);
         snackbar.GetComponentInChildren<Text>().text = "채팅방이 삭제됐습니다.";
+        Destroy(snackbar, DestroyTime);
+    }
+    public void Sb_Report()
+    {
+        GameObject snackbar = Instantiate(Resources.Load("Prefabs/SnackBar")) as GameObject;
+        snackbar.transform.SetParent(Canvas.transform, false);
+        snackbar.GetComponentInChildren<Text>().text = "신고가 접수됐습니다.";
+        Destroy(snackbar, DestroyTime);
+    }
+
+    public static void KW_Sb_DeleteList()
+    {
+        GameObject snackbar = Instantiate(Resources.Load("Prefabs/SnackBar")) as GameObject;
+        snackbar.transform.SetParent(Canvas_PersonalSeeMore.transform, false);
+        snackbar.GetComponentInChildren<Text>().text = "키워드는 최대 5개까지 선택 가능합니다.";
         Destroy(snackbar, DestroyTime);
     }
 }
