cb49e267bd7db2297d420c889e1c9303e8031075
kairos <momenting.dcd@gmail.com>
2022-06-07 14:00:57 +0900

    [마켓 서밋 빌드3 상태] 푸시

---
+++ gameChat/Assets/setstirgsdg.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class setstirgsdg : MonoBehaviour
+{
+    // Start is called before the first frame update
+   public void Start()
+    {
+        PlayerPrefs.SetString("GCName", "ÇöÁø");
+    }
+
+    
+}
