792a40047fdf3186c0dc48b89b7ec08312a550dd
Hyun <dltmdgus001@gmail.com>
2022-08-26 23:58:15 +0900

    [타인프로필보기]1차완성 테스트
    프리팹 만져야하는데 씬 겹칠까봐 아직 못함

--- gameChat/Assets/ElseProfileManager.cs
+++ gameChat/Assets/ElseProfileManager.cs
@@ -4,12 +4,20 @@ using UnityEngine;
 using FireStoreScript;
 using UnityEngine.UI;
 using System;
+using System.Text;
+
 
 namespace ElsePrefab { 
 public class ElseProfileManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+        public Text txtName;
+        public Text txtAge;
+        public Text txtIntro;
+        public Text txtSex;
+        public Text txtMbti;
+
+        // Start is called before the first frame update
+        void Start()
     {
         
     }
@@ -24,11 +32,26 @@ public class ElseProfileManager : MonoBehaviour
     public static string userName2;
     public void ElseProfile()
     {
-        userName.text = userName2;
+        //userName.text = userName2;
         GameObject.Find("PrCanvas").transform.Find("GC_Chat_PrCanvas").gameObject.SetActive(true);
             //
         FirebaseManager.ElseData(userName.text);
-    }
+        Debug.Log(userName.text + "유저네임");
+            txtName.text = userName.text;
+            txtAge.text = FirebaseManager.ElseAge;
+            txtMbti.text = FirebaseManager.ElseMbti;
+            txtIntro.text = FirebaseManager.Elseintroduction;
+            if (FirebaseManager.ElseSex == 1)
+            {
+                txtSex.text = "남";
+            }
+            else
+            {
+                txtSex.text = "여";
+            }
+            Debug.Log(txtName.text + txtAge.text + txtMbti.text+ txtIntro.text+ txtSex.text);
+        }
         
+
     }
 }
